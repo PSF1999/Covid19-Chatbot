ed1302fa8eb538306dda37d17ad80c0dc3e602b2
Praneeth Fernandez <praneethsf25@gmail.com>
2020-06-09 16:22:30 +0530

    first commit

---
+++ actions.py
@@ -0,0 +1,40 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+import requests
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionCoronaTracker(Action):
+
+    def name(self) -> Text:
+        return "action_corona_tracker"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        response = requests.get("https://api.covid19india.org/data.json").json()
+        
+        entities = tracker.latest_message['entities']
+        print("Last Message Now",entities)
+        state = None
+        for e in entities:
+            if e['entity'] == "state":
+                state = e['value']
+
+        for data in response["statewise"]:
+            if data["state"] == state.title():
+                active = data["active"]
+
+        dispatcher.utter_message(text="Total active cases in {} are {}".format(state.title(),active))
+
+        return []
---
+++ data/nlu.md
@@ -0,0 +1,29 @@
+## intent:greet
+- hey
+- hello
+- hi
+- good morning
+- good evening
+- hey there
+
+## intent:goodbye
+- bye
+- goodbye
+- see you around
+- see you later
+
+## intent:bot_challenge
+- are you a bot?
+- are you a human?
+- am I talking to a bot?
+- am I talking to a human?
+
+## intent: corona_state
+- [karnataka](state)
+- [kerala](state)
+- [delhi](state)
+- [maharashtra](state)
+- [Tamil nadu](state)
+- [andhra pradesh](state)
+- [assam](state)
+- [uttar pradesh](state)
